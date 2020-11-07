4042c3d54a44420382788b91788708a1bb72e2d0
ZackyAY <zackyazuardiyamani1324@gmail.com>
2020-11-07 14:57:08 +0700

    wordpress

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'sql12374923' );
+define( 'DB_NAME', 'idshoesid' );
 
 /** MySQL database username */
-define( 'DB_USER', 'sql12374923' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'SgAwu8I5fH' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'sql12.freesqldatabase.com' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
