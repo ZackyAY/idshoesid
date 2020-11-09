b1e3009364d1a81d57b64496e82568bdab7d4872
ZackyAY <zackyazuardiyamani1324@gmail.com>
2020-11-09 19:38:09 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -23,16 +23,16 @@ define('WP_CACHE', true); // Added by W3 Total Cache
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', '2JhFYum14m' );
+define( 'DB_NAME', 'idshoesid' );
 
 /** MySQL database username */
-define( 'DB_USER', '2JhFYum14m' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'H8wWwDB1qm' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'remotemysql.com' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
